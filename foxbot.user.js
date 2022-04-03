2fada2318ce2750bd583f4b8f6e9f52d151e5a21
mjokkie <66436642+mjokkie@users.noreply.github.com>
2022-04-03 12:44:52 +0200

    Update foxbot.user.js

--- foxbot.user.js
+++ foxbot.user.js
@@ -9,8 +9,8 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @require	     https://cdn.jsdelivr.net/npm/toastify-js
 // @resource     TOASTIFY_CSS https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css
-// @updateURL    https://github.com/mjokkie/fox/raw/main/foxbot.js
-// @downloadURL  https://github.com/mjokkie/fox/raw/main/foxbot.js
+// @updateURL    https://github.com/mjokkie/fox/raw/main/foxbot.user.js
+// @downloadURL  https://github.com/mjokkie/fox/raw/main/foxbot.user.js
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // ==/UserScript==
